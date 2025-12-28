e1636d630c51641c7cf03a9a2d311ebc063bca13
khushi <khushishorey2005@gmail.com>
2025-12-28 13:30:03 +0530

    Added all the 12 screens to the mobile app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./navigation/Navigation";
+
+// export default function App() {
+//   return (
+//    <View style={styles.root}>
+//      <Text>Hello Contributors!
+//      </Text>
+//    </View>
+//   );
+// }
 
 export default function App() {
-  return (
-   <View style={styles.root}>
-     <Text>Hello Contributors!
-     </Text>
-   </View>
-  );
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
